7e7f05212756f28473abf21cd1d638811c97397f
FmHasibul <fmhhasan@gmail.com>
2022-10-25 14:51:45 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+const courses = require('./courses.json')
+
 app.get('/', (req, res) => {
     res.send('Assignment API running')
 });
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
-app.listen(port, () => console.log(`assignment server running on${port}`))
+app.listen(port, () => console.log(`assignment server running on ${port}`))
 
 
 
