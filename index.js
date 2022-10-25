fca23382db6f8e23ea2d3c803d4ed37ebe30ded8
FmHasibul <fmhhasan@gmail.com>
2022-10-25 13:53:56 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Assignment API running')
+});
+
+app.listen(port, () => console.log(`assignment server running on${port}`))
 
 
 
