641980d70aad9639520a5cef96703b99acffd0a3
FmHasibul <fmhhasan@gmail.com>
2022-10-26 17:07:29 +0600

    data edited

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 const courses = require('./courses.json')
 
+app.use(cors())
+
 app.get('/', (req, res) => {
     res.send('Assignment API running')
 });
+
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const course = courses.find(course.id == id)
+    res.send(course)
+})
+
 
 app.listen(port, () => console.log(`assignment server running on ${port}`))
 
